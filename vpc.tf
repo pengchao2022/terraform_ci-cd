9eba791f232e073c68efb85df01312e1a01fb798
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2025-09-01 17:46:56 +0800

    updated

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,10 @@
 # 创建 VPC
 resource "aws_vpc" "main_vpc" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = "192.168.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
-    Name = "yiming-vpc"
+    Name = "allen-vpc"
   }
 }
