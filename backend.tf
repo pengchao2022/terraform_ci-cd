7e6259b53dd9fab5a13757538bae2b46c7411895
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2025-09-01 17:56:55 +0800

    updated

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraformstatefile090909"
-    key            = "s3pub_terraform.tfstate"
+    key            = "s3yimingpub_terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
