2598bfb5bdb569af4d439998adf2c5111ffb2c9f
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2025-09-02 07:50:15 +0800

    added s3 access

--- ec2.tf
+++ ec2.tf
@@ -16,6 +16,48 @@ resource "aws_iam_role" "ec2_role" {
   })
 }
 
+#创建一个独立的 IAM 策略，定义 S3 读写权限
+resource "aws_iam_policy" "s3_read_write_policy" {
+  name        = "EC2-S3-ReadWrite-Access"
+  description = "Allows read and write access to a specific S3 bucket"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:ListAllMyBuckets"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:ListBucket",
+          "s3:GetBucketLocation"
+        ]
+        Resource = "arn:aws:s3:::terraformstatefile090909" # 替换为你的桶名
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject",
+          "s3:PutObjectAcl"
+        ]
+        Resource = "arn:aws:s3:::terraformstatefile090909/*" # 替换为你的桶名，/* 表示桶内所有对象
+      }
+    ]
+  })
+}
+#为 IAM 角色附加 指定s3桶读写策略
+resource "aws_iam_role_policy_attachment" "s3_policy" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "aws_iam_policy.s3_read_write_policy.arn"
+}
+
 # 为 IAM 角色附加 AmazonSSMManagedInstanceCore 策略
 resource "aws_iam_role_policy_attachment" "ssm_policy" {
   role       = aws_iam_role.ec2_role.name
